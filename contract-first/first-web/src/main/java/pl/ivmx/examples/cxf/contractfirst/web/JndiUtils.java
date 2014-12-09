ba07db88629299c4792966fd39896af0ada36c2c
Beniamin Czaplicki <beniamin.czaplicki@gmail.com>
2014-12-09 16:18:59 +0100

    commit module contract-first service and web module

---
+++ contract-first/first-web/src/main/java/pl/ivmx/examples/cxf/contractfirst/web/JndiUtils.java
@@ -0,0 +1,30 @@
+package pl.ivmx.examples.cxf.contractfirst.web;
+
+import org.apache.log4j.Logger;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * @author bczaplicki@ivmx.pl, IVMX 2014.
+ */
+public class JndiUtils {
+
+    private static final Logger log = Logger.getLogger(JndiUtils.class);
+    /**
+     * Gets local EJB from JNDI.
+     *
+     * @param jndiName JNDI constant name to look up for EJB
+     * @param <T> generic object
+     * @return local EJB object loaded from JNDI
+     */
+    public static <T> T getLocalEJB(String jndiName) {
+        try {
+            InitialContext context = new InitialContext();
+            return (T) context.lookup(jndiName);
+        } catch (NamingException e) {
+            log.error("Naming exception occurred while trying to load EJB from JNDI with JNDI name: " + jndiName, e);
+            throw new RuntimeException("Naming exception occurred while trying to load EJB from JNDI with JNDI name: " + jndiName, e);
+        }
+    }
+}
